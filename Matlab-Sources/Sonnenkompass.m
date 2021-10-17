a75f5a2055d9ce5db141d3757f3eb49eb448a98a
JW-Schuetz <jws1954@gmx.de>
2021-10-17 12:22:01 +0200

    Aktualisierung

--- Matlab-Sources/Sonnenkompass.m
+++ Matlab-Sources/Sonnenkompass.m
@@ -1,18 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Lösung des Problems "Sonnenkompass"
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc
 clear
 
+% Symbol-Definitionen
 Phi = sym( 'Phi' ); % Winkel Erd-Rotationsachse zur Ekliptik
+RS  = sym( 'RS' );  % Abstand Erde - Sonne
+RE  = sym( 'RE' );  % Erdradius
+L   = sym( 'L' );   % Stablänge
 
-n      = sym( 'n', [ 3, 1 ] );	% Einheitsvektor Rotationsachse 
+% Einheitsvektor Rotationsachse
+n      = sym( 'n', [ 3, 1 ] );
 n( 1 ) = sin( Phi );
 n( 2 ) = 0;
 n( 3 ) = cos( Phi );
 
-Alpha = sym( 'Alpha' );	% Erd-Rotationswinkel (2*pi/24h)
+% Erd-Rotationswinkel (2*pi/24h)
+Alpha = sym( 'Alpha' );
 ca    = cos( Alpha );
 sa    = sin( Alpha );
 
-DAlpha = sym( 'DAlpha', [ 3, 3 ] );	% Drehmatrix
+% Drehmatrix
+DAlpha = sym( 'DAlpha', [ 3, 3 ] );
+
 DAlpha( 1, 1 ) = n( 1 )^2        * ( 1 - ca ) + ca;             % 1. Spalte
 DAlpha( 2, 1 ) = n( 1 ) * n( 2 ) * ( 1 - ca ) + n( 3 ) * sa;
 DAlpha( 3, 1 ) = n( 1 ) * n( 3 ) * ( 1 - ca ) - n( 2 ) * sa;
@@ -25,4 +36,20 @@ DAlpha( 1, 3 ) = n( 1 ) * n( 3 ) * ( 1 - ca ) + n( 2 ) * sa;    % 3. Spalte
 DAlpha( 2, 3 ) = n( 2 ) * n( 3 ) * ( 1 - ca ) - n( 1 ) * sa;
 DAlpha( 3, 3 ) = n( 3 )^2        * ( 1 - ca ) + ca;
 
-P = sym( 'P', [ 3, 1 ] );	% Punkt P der Erdoberfläche
+DAlpha = simplify( DAlpha );    % Vereinfachung möglich?
+
+% Fusspunkt des Stabes auf der Erdoberfläche und Stabende
+P = sym( 'p', [ 3, 1 ] );
+Q = ( 1 + L / RE ) * P;
+
+% rotierte Punkte P und Q
+PAlpha = DAlpha * P;
+QAlpha = DAlpha * Q;
+
+% Sonne
+S = sym( 's', [ 3, 1 ] );
+S( 1 ) = RS;
+S( 2 ) = 0;
+S( 3 ) = 0;
+
+i = 1
