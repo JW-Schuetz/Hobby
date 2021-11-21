9ba7f2f10a49ed26a6776fd3fc6b4fe60a2ce09e
JW-Schuetz <jws1954@gmx.de>
2021-11-21 22:30:00 +0100

    Aktualisierung

--- Matlab-Sources/TigonometrischerTest.m
+++ Matlab-Sources/TigonometrischerTest.m
@@ -10,9 +10,12 @@ h = ( rE + lS ) * sin( alpha );
 d = ( rE + lS ) * cos( alpha ) - rE;
 H = sqrt( h^2 + ( sE - rE - d )^2 );
 
-x = simplify( lS * tan( asin( h / H ) ) );
+x = simplify( lS * tan( alpha + asin( h / H ) ) );
 
-x = subs( x, 'alpha', ( 28.136746041614316 - 23.44 ) / 180.0 * pi );
+breite = 28.136746041614316 / 180.0 * pi;
+psi    = 23.44 / 180.0 * pi;
+
+x = subs( x, 'alpha', breite - psi );
 x = subs( x, 'lS', 1.5 );
 x = subs( x, 'rE', 6371000.8 );
 x = subs( x, 'sE', 149597870700.0 );
