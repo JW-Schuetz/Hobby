70d4e40352f4dc37d1e44d06d60803331aec0595
JW-Schuetz <jws1954@gmx.de>
2021-11-27 21:24:22 +0100

    Aktualisierung

--- Matlab-Sources/SonnenkompassPlot.m
+++ Matlab-Sources/SonnenkompassPlot.m
@@ -2,8 +2,29 @@ function SonnenkompassPlot()
     clc
     clear
 
+    ort   = 'LasPalmas';
+    datum = '21.12.2021';
+
+	switch( ort )
+        case 'LasPalmas'
+            switch datum
+                case '12.10.2021'
+                    arrowType  = '\downarrow';
+                    yArrow     = 1.3;
+                    yUhrZeit   = 1.4;
+                    squareSize = 2;
+
+                case '21.12.2021'
+                    arrowType  = '\uparrow';
+                    yArrow     = 1.7;
+                    yUhrZeit   = 1.4;
+                    squareSize = 6;
+            end
+        case 'Hoechst'
+	end
+
     % Ergebnisdaten laden
-    load( 'LasPalmas-12.10.2021.mat', 'y' )
+    load( [ ort, '-', datum, '.mat' ], 'y' )
 
     % Aufräumen
     ndx = ~isnan( y( :, 2 ) );
@@ -11,7 +32,12 @@ function SonnenkompassPlot()
 
     N = size( y, 1 );
 
-%     minAbstand = min( sqrt( y( :, 2 ).^2 + y( :, 3 ).^2 ) )
+    % Minimaler Abstand Stab <-> Schattenende
+    [ ~, minNdx ] = min( sqrt( y( :, 2 ).^2 + y( :, 3 ).^2 ) );
+    minH          = y( minNdx, 2 ); % y1 des Minimums
+    % y1-Achse verschieben um minH, d.h. Minimum ist bei y1=0 und lineare
+    % Interpolation der y2-Werte
+	y( :, 3 ) = interp1( y( :, 2 ), y( :, 3 ), y( :, 2 ) + minH );
 
     figure
     title( 'Schattentrajektorie' )
@@ -23,19 +49,18 @@ function SonnenkompassPlot()
     ylabel( 'Süd-Nord [m]' )
 
     % Darstellungsbereich in Metern
-    squareSize = 6;     % [m]
-    xlim( squareSize * [ -1, 1 ] );
+    xlim( squareSize * [ -1,   1 ] );
     ylim( squareSize * [ -0.1, 1 ] );
 
     % Ort des Stabes plotten
     plot( 0, 0, 'o', 'MarkerSize', 10, 'MarkerFaceColor', 'r' )
-    % Schatten-Trajektorie plotten
-    mndx = 1 : 10 : N;
+    % Schatten-Trajektorie plotten, Markerindizes zentrisch ausrichten
+    mndx = [ minNdx : -10 : 1, minNdx + 10 : 10 : N ];
     plot( y( :, 2 ), y( :, 3 ), '-o', 'MarkerSize', 3, 'MarkerIndices', mndx, ...
         'Color', 'k', 'LineWidth', 1 )
     % Markierung 12 Uhr
-	text( 0, 1.7, '\uparrow', 'HorizontalAlignment', 'center' )
-	text( 0, 1.3, '12:00 Uhr', 'HorizontalAlignment', 'center' )
+	text( 0, yArrow, arrowType, 'HorizontalAlignment', 'center' )
+	text( 0, yUhrZeit, '12:00 Uhr', 'HorizontalAlignment', 'center' )
 
     legend( 'Stabposition', 'Trajektorie, 10 Minuten-Intervalle' )
 end
