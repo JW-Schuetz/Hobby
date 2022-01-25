0ad8a8c1de5a6b2ee2ced9b851a89dcf496b240c
JW-Schuetz <jws1954@gmx.de>
2022-01-25 23:12:22 +0100

    Aktualisierung

--- Matlab-Sources/SonnenkompassSymbolicAddon.m
+++ Matlab-Sources/SonnenkompassSymbolicAddon.m
@@ -2,8 +2,14 @@ clc
 clear
 
 syms alpha real
+syms psi real
+syms omega real
 syms lS real
 syms rE real
+syms rS real
+syms p1 real
+syms p2 real
+syms p3 real
 syms s real
 syms c real
 
@@ -36,5 +42,40 @@ C = simplify( z - A * s - B * c );
 probe = simplify( lS * rE * omegaS - A * sin( alpha ) - ...
                   B * cos( alpha ) - C );
 if( probe ~= 0 )
-    error( 'Probe gescheitert!' )
+    error( 'Probe A, B, C gescheitert!' )
+end
+
+A, B, C
+
+% Ausgabe diese Skripts:
+% ======================
+% A = p3*rS*sin(omega)*sin(psi) + p2*rS*cos(omega)*cos(psi) - 
+%     p1*rS*cos(psi)*sin(omega)
+% B = p3*rS*cos(omega)*cos(psi)*sin(psi) - p1*rS*cos(omega)*cos(psi)^2 - 
+%     p2*rS*sin(omega)
+% C = rE^2 - p1*rS*cos(omega) + p1*rS*cos(omega)*cos(psi)^2 - 
+%     p3*rS*cos(omega)*cos(psi)*sin(psi)
+
+% Kontrolle der Formeln in Sonnenkompass.lyx
+A = subs( A, p2, 0 );
+B = subs( B, p2, 0 );
+C = subs( C, p2, 0 );
+
+AS = - rS * ( p1 * cos( psi ) - p3 * sin( psi ) ) * sin( omega );
+BS = - rS * ( p1 * cos( psi ) - p3 * sin( psi ) ) * cos( psi ) * cos( omega );
+CS = rE^2 - rS * ( p1 * sin( psi )^2 + p3 * cos( psi ) * sin( psi ) ) * cos( omega );
+
+probe = simplify( A - AS );
+if( probe ~= 0 )
+    error( 'LyX Probe A gescheitert!' )
+end
+
+probe = simplify( B - BS );
+if( probe ~= 0 )
+    error( 'LyX Probe B gescheitert!' )
+end
+
+probe = simplify( C - CS );
+if( probe ~= 0 )
+    error( 'LyX Probe C gescheitert!' )
 end
